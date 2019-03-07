0951b31127025f90d800faf0f764179c2274cf46
Arvind SN <asn@asn.com>
2019-03-07 14:47:15 +0530

    Adding Security and Security Entities

--- Bookstore/src/main/java/com/bookstore/domain/User.java
+++ Bookstore/src/main/java/com/bookstore/domain/User.java
@@ -1,13 +1,29 @@
 package com.bookstore.domain;
 
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.bookstore.domain.security.Authority;
+import com.bookstore.domain.security.Role;
+import com.bookstore.domain.security.UserRole;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
-public class User {
+public class User implements UserDetails{
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	@Column(name="id", nullable = false, updatable = false)
@@ -21,6 +37,10 @@ public class User {
 	private String phone;
 	private boolean enabled=true;
 	
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	@JsonIgnore // ignore this fields when serializing an object to JSON. 
+	private Set<UserRole> userRoles = new HashSet<>();
+
 	public Long getId() {
 		return id;
 	}
@@ -63,10 +83,47 @@ public class User {
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
-	public boolean isEnabled() {
-		return enabled;
-	}
+	
 	public void setEnabled(boolean enabled) {
 		this.enabled = enabled;
 	}
+	public Set<UserRole> getUserRoles() {
+		return userRoles;
+	}
+	public void setUserRoles(Set<UserRole> userRoles) {
+		this.userRoles = userRoles;
+	}
+	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		Set<GrantedAuthority> authorites = new HashSet<>();
+		for (UserRole ur : userRoles) {
+		    Role role = ur.getRole();
+			String authority_name = role.getName();
+			Authority ua = new Authority(authority_name);
+			authorites.add(ua);
+		}
+		
+		// This is a concise way of writing the traditional for loop
+		//userRoles.forEach(ur -> authorites.add(new Authority(ur.getRole().getName())));
+
+		return authorites;
+	}
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+	@Override
+	public boolean isEnabled() {
+		return enabled;
+	}
+	
 }
