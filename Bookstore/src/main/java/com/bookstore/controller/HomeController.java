d5e3fa32fb4116f58a8deb75ce055f3f3715ca01
Arvind SN <asn@asn.com>
2019-03-07 17:40:38 +0530

    Wiring up security configuration and entities

--- Bookstore/src/main/java/com/bookstore/controller/HomeController.java
+++ Bookstore/src/main/java/com/bookstore/controller/HomeController.java
@@ -1,7 +1,11 @@
 package com.bookstore.controller;
 
+import java.util.Locale;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
@@ -9,9 +13,29 @@ public class HomeController {
 	public String index() {
 		return "index";
 	}
+
+//  Not required. Handled by other controllers
+//	@RequestMapping("/myAccount")
+//	public String myAccount() {
+//		return "myAccount";
+//	}
 	
-	@RequestMapping("/myAccount")
-	public String myAccount() {
+	@RequestMapping("/login")
+	public String login(Model model) {
+		model.addAttribute("classActiveLogin", true);
 		return "myAccount";
 	}
+	
+	@RequestMapping("/forgotPassword")
+	public String forgetPassword(Model model) {		
+		model.addAttribute("classActiveForgotPassword", true);
+		return "myAccount";
+	}
+	
+	@RequestMapping("/newUser")
+	public String newUser(Model model) {
+		model.addAttribute("classActiveNewAccount", true);
+		return "myAccount";
+	}
+
 }
