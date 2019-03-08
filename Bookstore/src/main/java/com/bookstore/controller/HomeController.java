56c04025614f5aeae64c05699bca77219c1558e1
Arvind SN <asn@asn.com>
2019-03-08 12:01:32 +0530

    Adding PasswordResetToken entity, repo and UserService

--- Bookstore/src/main/java/com/bookstore/controller/HomeController.java
+++ Bookstore/src/main/java/com/bookstore/controller/HomeController.java
@@ -2,13 +2,20 @@ package com.bookstore.controller;
 
 import java.util.Locale;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.bookstore.domain.security.PasswordResetToken;
+import com.bookstore.service.UserService;
+
 @Controller
 public class HomeController {
+	@Autowired
+	private UserService userService;
+
 	@RequestMapping("/")
 	public String index() {
 		return "index";
@@ -33,7 +40,8 @@ public class HomeController {
 	}
 	
 	@RequestMapping("/newUser")
-	public String newUser(Model model) {
+	public String newUser(Locale locale, @RequestParam("token") String token, Model model) {
+		PasswordResetToken passToken = userService.getPasswordResetToken(token);
 		model.addAttribute("classActiveNewAccount", true);
 		return "myAccount";
 	}
