5525bb6c47a8f669a678c5cfc35575d3704fd6ce
Arvind SN <asn@asn.com>
2019-03-14 11:15:04 +0530

    Add the book service and repository layer functionality

---
+++ Bookstore/src/main/java/com/bookstore/service/BookServiceImpl.java
@@ -0,0 +1,22 @@
+package com.bookstore.service;
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bookstore.domain.Book;
+import com.bookstore.repository.BookRepository;
+
+@Service
+public class BookServiceImpl implements BookService {
+	@Autowired
+	private BookRepository bookRepository;
+
+	@Override
+	public List<Book> findAll() {
+		return bookRepository.findAll();
+	}
+
+}
