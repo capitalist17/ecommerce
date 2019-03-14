2d64544b80cee7a0f977342ca95ef5889615b86a
Arvind SN <asn@asn.com>
2019-03-14 14:08:35 +0530

    Add findById to book service

--- adminportal/src/main/java/com/adminportal/service/BookServiceImpl.java
+++ adminportal/src/main/java/com/adminportal/service/BookServiceImpl.java
@@ -23,4 +23,9 @@ public class BookServiceImpl implements BookService {
 		return (List<Book>) bookRepository.findAll();
 	}
 
+	@Override
+	public Book findById(Long id) {
+		return bookRepository.findById(id).get();
+	}
+
 }
