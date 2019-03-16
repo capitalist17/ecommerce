1c894d72bd64ec3736f31e506c3766dda30bba0f
Arvind SN <asn@asn.com>
2019-03-16 18:28:38 +0530

    Change the Id generation from AUTO to IDENTITY

--- Bookstore/src/main/java/com/bookstore/domain/BillingAddress.java
+++ Bookstore/src/main/java/com/bookstore/domain/BillingAddress.java
@@ -10,7 +10,7 @@ import javax.persistence.OneToOne;
 public class BillingAddress {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String BillingAddressName;
 	private String BillingAddressStreet1;
