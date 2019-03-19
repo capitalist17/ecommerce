4142151a54a21561ff16a875ff57207ef0326da6
Arvind SN <asn@asn.com>
2019-03-19 12:11:46 +0530

    Add shipping, payment and billing same as shipping controls

--- Bookstore/src/main/resources/static/js/scripts.js
+++ Bookstore/src/main/resources/static/js/scripts.js
@@ -1,3 +1,10 @@
+function checkBillingAddress() {
+	if($("#theSameAsShippingAddress").is(":checked")) {
+		$(".billingAddress").prop("disabled", true);
+	} else {
+		$(".billingAddress").prop("disabled", false);
+	}
+}
 
 $(document).ready(function(){
 	$(".cartItemQty").on('change', function(){
@@ -5,4 +12,7 @@ $(document).ready(function(){
 		
 		$('#update-item-'+id).css('display', 'inline-block');
 	});
+	
+	$("#theSameAsShippingAddress").on('click', checkBillingAddress);
+
 });
